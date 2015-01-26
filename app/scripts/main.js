00cf97f9bd7c7209269fd7354354dddab6d82851
Michael Kohler <mkohler@picobudget.com>
2015-01-26 19:36:13 +0100

    some test content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,5 @@
 (function () {
     'use strict';
 
-    document.querySelector('#testbutton').addEventListener('click', function () {
-       alert('foo!!');
-    });
 
-    document.querySelector('#js-warning').setAttribute('hidden', 'hidden');
 }());

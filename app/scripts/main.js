e6adda5ac7f29404c1d96e68722a4f5158a68e23
Michael Kohler <mkohler@picobudget.com>
2015-02-28 00:48:18 +0100

    add l10n

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,14 @@
 (function () {
     'use strict';
     $(document).foundation();
+    document.webL10n.ready(function() {
+        var lang = document.webL10n.getLanguage().substring(0, 2);
+
+        if (lang == "en") {
+            document.getElementById('language').value = "en";
+        }
+        if (lang == "de") {
+            document.getElementById('language').value = "de";
+        }
+    });
 })();
